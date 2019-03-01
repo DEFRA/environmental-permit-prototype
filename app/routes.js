95076c5f1476c3b506ddd7b0992beb9f5f785c05
christinagyles_ea <christina.gyles@environment-agency.gov.uk>
2019-03-01 08:51:08 +0000

    updating routes for version 24 in new kit app, updating guide page to run html menu and search box

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,19 @@
-const express = require('express')
-const router = express.Router()
+var express = require('express')
+var router = express.Router()
 
-// Add your routes here - above the module.exports line
+router.use(function (req, res, next) {
+  res.locals.hostname=process.env.THISHOST
+  next()
+});
+
+
+// anything beginning with "/vn" will go into this
+router.use('/v24', require('./routes_v24'))
+
+
+// Route index page
+router.get('/', function (req, res) {
+  res.render('index')
+})
 
 module.exports = router
