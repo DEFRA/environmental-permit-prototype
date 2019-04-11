95d7ece01dcb8b970b59340378ba0a866082be53
Martin Glancy <martin.glancy@environment-agency.gov.uk>
2019-04-11 14:08:11 +0100

    Remove public folder

--- public/v6/javascripts/routing/join.js
+++
@@ -1,5 +0,0 @@
-// JSON object merge using JQuery
-
-var data = $.extend(true, topleveldata, wastedata); // true = "deep" parameter of jquery extend https://api.jquery.com/jquery.extend/
-
-//var data = topleveldata;
